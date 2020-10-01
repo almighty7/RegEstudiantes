283c324e8be44ed239461dd7697af5b9672bbc9c
almighty7 <38875101+almighty7@users.noreply.github.com>
2020-09-30 22:59:13 -0500

    v2

--- app/src/main/java/com/example/regestudiantes/MainActivity.java
+++ app/src/main/java/com/example/regestudiantes/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.regestudiantes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText nombres, apellidos,dni,cui,telefono;
+    Button aceptar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombres=(EditText)findViewById(R.id.editNombres);
+        apellidos=(EditText)findViewById(R.id.editApellidos);
+        dni=(EditText)findViewById(R.id.editDni);
+        cui=(EditText)findViewById(R.id.editCui);
+        telefono=(EditText)findViewById(R.id.editTelefono);
+        aceptar=(Button)findViewById(R.id.btnGuardar);
+
+        aceptar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+            }
+        });
     }
 }
